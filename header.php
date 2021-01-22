d2ba1dff3023a11b742d06d5013bf3400bb47a09
BAVA SHIEK BAREETH <bava.tealorca@gmail.com>
2021-01-22 13:16:16 +0530

    php

---
+++ header.php
@@ -0,0 +1,34 @@
+<div class="fixed-top">
+<div class="header-top">
+	<div class="row" style="height:35px;padding:5px">
+		<div class="col-sm12 col-12 text-center text-sm-right">
+			<ul class="list-unstyled">
+				<li class="list-inline-item">Covid-19 (coronavirus): a message from Wheel Mode<a href="https://wheelmode.com/en/covid19_message" style="color:red;">Read more &nbsp</a></li>
+				<li class="list-inline-item hide-mbl"><a href="tel:+971 54444803">+971 54444803</a></li>
+				<li class="list-inline-item hide-mbl"><a href="https://wheelmode.com/en/login">Login/signup</a></li>
+				<li class="list-inline-item hide-mbl dropbtn"><a href="https://wheelmode.com/en/about" style ="margin-right:100px;">About</a></li>
+			</ul>
+		</div>
+	</div>
+</div>
+<div class="header-bottom">
+	<div class="row" style="margin-left:50px;height:100px;margin-right:10%;" >
+		<div class="col-sm12 col-12 text center text-sm-right">
+			<img src="logo.png" alt="wheelmode" style="float:left;margin-top:10px;margin-left:10%;"><a href="https://wheelmode.com/en/"></a>
+			<ul class="list-unstyled" style="margin-top:15px;color:black;">
+				<li class="list-inline-item"><a href="https://wheelmode.com/en/tyres/search/?productStock=1">Tyres &nbsp;&nbsp;&nbsp;</a></li>
+				<li class="list-inline-item dropdown"><button type="button" class="btn bg-white dropdown-toggle" data-toggle="dropdown">Products&nbsp;&nbsp;&nbsp;</button>
+					<ul class="dropdown-menu">
+						<li class="dropdown-item"><a href="https://wheelmode.com/en/coming-soon">Break</a></li>
+						<li class="dropdown-item"><a href="https://wheelmode.com/en/coming-soon">Battaries</a></li>
+					</ul>
+				</li>
+				<li class="list-inline-item"><a href="https://wheelmode.com/en/installersnetwork">Installer Network &nbsp;&nbsp;&nbsp;</a></li>
+				<li class="list-inline-item"><a href="https://wheelmode.com/en/services">Services&nbsp;&nbsp;&nbsp;</a></li>
+				<li class="list-inline-item"><a href="https://wheelmode.com/en/special-offers">Special Offers&nbsp;&nbsp;&nbsp;</a></li>
+				<li class="list-inline-item"><a href="https://wheelmode.com/en/brands">Our Brands&nbsp;&nbsp;&nbsp;</a></li>
+				<li class="list-inline-item"><a href="https://wheelmode.com/en/cart/">Cart</a></li>
+			</ul>
+		</div>
+	</div>		
+</div>
---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<?php include 'head.php';?>
+</head>
+<header class="container-fluid">
+	<?php include 'header.php';?>
+</header>
+<body>
+	<div class="text-center">
+		<h2>Our 4 Step Process</h2>
+		<p>The easiest way to search and buy new tyres</p>
+	</div>
+	<?php include 'container.php';?>
+	<div class="text-center">
+		<h2>Our Premium list of Brands</h2>
+		<p>Over 300,000 Satisfied Customers Drive On Our Tires Everyday</p>
+	</div>
+</body>
+</html>
