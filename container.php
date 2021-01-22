14e9fb2fe825489ff68ca91f29de7ad6e2135fb8
BAVA SHIEK BAREETH <bava.tealorca@gmail.com>
2021-01-22 13:47:04 +0530

    container

---
+++ container.php
@@ -0,0 +1,29 @@
+<div class="container">
+	<div class="row">
+		<div class="col-sm">
+			<img src="1.png" alt="tyres"><h4>Search & select your tyres</h4>
+			<p>We stock a wide range of online tyres for all vehicles Choose Installation mode</p></img>
+		</div>
+		<div class="col-sm-1">
+			<div class="arrows"></div>
+		</div>
+		<div class="col-sm">
+			<img src="2.png" alt="install"><h4>Choose Installation mode</h4>
+			<p>You can select 100+ Installers across UAE, you can also choose mobile fitment or home delivery</p>
+		</div>
+		<div class="col-sm-1">
+			<div class="arrows"></div>
+		</div>
+		<div class="col-sm">
+			<img src="3.png" alt="Payment"><h4>Secure online payment</h4>
+			<p>We ensure you that our payment gateways are 100% safe</p>
+		</div>
+		<div class="col-sm-1">
+			<div class="arrows"></div>
+		</div>
+		<div class="col-sm">
+			<img src="4.png" alt="Delivery"><h4>Delivery & Installation</h4>
+			<p>Within 24hrs your tyres will be delivered to your select location</p>
+		</div>
+	</div>
+</div>
